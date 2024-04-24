955cd816cd4ae1c17c09b0dda018a3f3b564f762
pradhumgp <pradhumgp@gmail.com>
2024-04-24 22:41:27 +0530

    wrote register user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,9 @@
 import { syncIndexes } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -12,8 +16,60 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for user creation
   // return response
 
-  const { username, email, passowrd, fullName } = req.body;
+  const { username, email, password, fullName } = req.body;
   console.log(email, username);
+
+  if([fullName, email, username, password].some((field)=>{
+    return field?.trim() === ""
+  })){
+    throw new apiError(400, "All Fields are required")
+  }
+
+  const existingUser = User.findOne({
+    $or: [{ username },{ email }]
+  })
+
+  if(existingUser){
+    throw new apiError(409, "User with email ir username already exists")
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath){
+    throw new apiError(400, "Avatar File is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if(!avatar){
+    throw new apiError(400, "Avatar File is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  })
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  if(!createdUser){
+    throw new apiError(500, "Something went wrong while registering the user");
+  }
+
+  return res.status(201).json(
+    new apiResponse(200, createdUser, "User registered successfully")
+  )
+
+
+
 });
 
 export { registerUser };
