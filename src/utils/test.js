39c70b9839bd35b860c8d29a62fd96bdab25640c
Pradhum Gupta <69038235+pradhumgp@users.noreply.github.com>
2025-01-15 23:49:47 +0530

    Update test.js

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,26 +1,10 @@
-function groupByXYZAndAddFields(data, additionalFields, fieldMapping) {
-  // Rename fields based on fieldMapping
-  const renamedData = data.map((item) => {
-    const renamedItem = { ...item };
-    for (const [oldField, newField] of Object.entries(fieldMapping)) {
-      if (oldField in renamedItem) {
-        renamedItem[newField] = renamedItem[oldField];
-        delete renamedItem[oldField]; // Remove the old field
-      }
+function getUniqueByField(data, field) {
+  const seen = new Set();
+  return data.filter(item => {
+    if (seen.has(item[field])) {
+      return false;
     }
-    return renamedItem;
+    seen.add(item[field]);
+    return true;
   });
-
-  // Group objects by the new field (e.g., 'category')
-  const groupedMap = renamedData.reduce((acc, obj) => {
-    const key = obj.category; // Adjust based on your renamed key
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    acc[key].push({ ...obj, ...additionalFields });
-    return acc;
-  }, {});
-
-  // Convert grouped map to an array of arrays
-  return Object.values(groupedMap);
 }
