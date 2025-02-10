934065bbe7fbca027bee8b9f46a3f3d1fa2654de
Pradhum Gupta <69038235+pradhumgp@users.noreply.github.com>
2025-02-10 23:06:09 +0530

    Update test.js

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,14 +1,18 @@
-function mergeAndMap(obj1, obj2, mapping = {}) {
-    let merged = { ...obj1, ...obj2 };
+import { LoginCallback } from '@okta/okta-react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-    // Apply mapping
-    Object.keys(mapping).forEach((oldKey) => {
-        if (Object.prototype.hasOwnProperty.call(merged, oldKey)) {
-            const newKey = mapping[oldKey];
-            merged[newKey] = merged[oldKey];
-            delete merged[oldKey]; // Remove old key
-        }
-    });
+const CustomLoginCallback = () => {
+  const navigate = useNavigate();
 
-    return merged;
-}
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/'); // Redirect to home if an error occurs
+    }, 2000); // Adjust time as needed
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return <LoginCallback errorComponent={() => navigate('/')} />;
+};
+
+export default CustomLoginCallback;
